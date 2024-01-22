002d4e582e9f48babb4b19e532790b41c0223c3f
RuunStreur <ruunstreur@gmail.com>
2024-01-22 16:14:09 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome! Please use a laptop and click the fullscreen button for a better experience.</h1>
 <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="600" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FVsi11KiJS3DWtPrujEnCq0%2FDSL-Rijksmuseum%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D4-16%26viewport%3D271%252C174%252C0.12%26t%3D87uRYCGnXLdaU7uC-1%26scaling%3Dscale-down%26starting-point-node-id%3D4%253A16%26show-proto-sidebar%3D1%26mode%3Ddesign" allowfullscreen></iframe>
+
+<style>
+    h1{
+        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: #312314;
+    }
+</style>
